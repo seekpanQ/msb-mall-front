caedcd6fac37000d2f5bcdcf14a7c7e3a6efb09e
seekpan <lixin_qiu@163.com>
2024-09-25 17:35:22 +0800

    动态获取树形菜单

--- renren-fast-vue-master/src/views/modules/product/category.vue
+++ renren-fast-vue-master/src/views/modules/product/category.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-        6666
-    </div>
+  <div>
+      <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  </div>
 </template>
 <script>
-export default {
-
-}
+/* eslint-disable */
+  export default {
+    data() {
+      return {
+        data: [],
+        defaultProps: {
+          children: 'children',
+          label: 'name'
+        }
+      };
+    },
+    methods: {  
+       getCategory(){
+        this.$http({
+          url: this.$http.adornUrl('/product/category/listTree'),
+          method: 'get'
+        }).then(({data}) => {
+          console.log("成功获取的类别数据：",data.data)
+          this.data = data.data
+        })
+      },
+      handleNodeClick(data) {
+        console.log(data);
+      }
+    },created() {
+        this.getCategory();
+    }
+  };
 </script>
-<style>
-</style>
